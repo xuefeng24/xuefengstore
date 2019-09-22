d50c3f957e36370717acec21b49cfbe59d612db7
xuefeng <1418896645@qq.com>
2019-09-22 20:48:46 +0800

    现在是20:48

---
+++ eureka/src/main/java/com/xuefeng/eureka/User.java
@@ -0,0 +1,12 @@
+package com.xuefeng.eureka;
+
+/**
+ * author:xuefeng
+ * time:2019/9/22 20:40
+ * describe
+ */
+public class User {
+    public void  getUserById(){
+        System.out.println("你是什么捏");
+    }
+}
